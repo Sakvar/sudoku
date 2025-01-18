a7f6fef4cf246d67e2f3566277dbf85f25e5e9b5
Sakvar <sakvar@gmail.com>
2025-01-17 22:26:57 -0500

    possible iphone fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,7 @@ export default function RootLayout({
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no" />
         {process.env.NEXT_PUBLIC_TELEGRAM_WEBAPP === 'true' && (
           <Script 
             src="https://telegram.org/js/telegram-web-app.js"
